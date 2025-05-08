d14b99c1ed22e82cad00cce7bb9420c772f895f6
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-08 15:40:52 +0700

    bool delnode

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -84,5 +84,24 @@ public:
             *previous = *current;
             *current = (*current)->next;
         }
-    }
+        
+            return (*current != NULL);
+        }
+
+        bool delnode(int nim)
+        {
+            node *current, *previous;
+            if (!search(nim, &previus, &current))
+                return false;
+
+            if (current == START)
+                START = START-> next;
+            else
+                previous -> next = current -> next;
+
+            delete current;
+            return true;
+        }
+
+        void traverse()
     }
