a159b0e5c6c00bf192292354a2b515860af120d2
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-08 16:34:34 +0700

    save

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -131,7 +131,65 @@ public:
         cout << "4.mencari data dalam list" << endl;
         cout << "5.keluar" << endl;
         cout 
-             << "masukkan pilihan (1-5)"; << endl;
-            cin >> ch;
+             << "masukkan pilihan (1-5)"<< endl;
+        cin >> ch;
+        switch (ch)
+        {
+            mhs.addnode();
+            break;
+        }
+
+        case '2':
+        {
+            if (mhs.listempty())
+            {
+              cout << endl
+              <<"list kosong"<< endl;
+              break;
+            }
+            cout << endl
+            <<"\nmasukkan no mahasiswa yang akan dihapus:";
+            cin >> nim;
+            if (mhs.delnode(nim)== false)
+            cout << endl
+            <<"data tidak ditemukan" << endl;
+            else
+            cout << endl
+            <<"data dengan nomor mahasiswa" << nim << "berhasil dihapus"<< endl;
+        }
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+
+        case '4':
+        {
+            if (mhs.listempty()== true)
+            {
+                cout << "\nlist kosong\n";
+                break;
+            }
+            node *previous , *current;
+            cout << endl
+            <<"masukkan no mahasiswa yang dicari:";
+            cin >> nim;
+            if (mhs.search(nim, &previous, &current)== false)
+            cout << endl
+            <<"data ditemukan" << endl;
+            cout << "\nNo mahasiswa:"<< current -> nomhs << endl;
+            cout << "\n";
+        }
+
     }
- }
+    break;
+    case '5';
+    {
+        cout << "pilihan salah!"<< endl;
+    }
+    {
+        break;
+    }
+ }while (ch != '5');
+}
